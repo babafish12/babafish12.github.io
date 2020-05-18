38425be254a1fadf5f3a57ff9958e8929969d8b3
babafish <59877153+babafish12@users.noreply.github.com>
2020-05-18 20:56:43 +0200

    sdfs

--- Unterseiten/skript.js
+++ Unterseiten/skript.js
@@ -16,3 +16,19 @@ new Darkmode(options).showWidget();
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
