e7c61b8adf26238c623d9ba4c8ee5ff52794279d
babafish <59877153+babafish12@users.noreply.github.com>
2020-05-18 15:35:35 +0200

    sdf

--- Unterseiten/skript.js
+++ Unterseiten/skript.js
@@ -8,7 +8,7 @@ const options = {
     buttonColorDark: '#100f2c',  // default: '#100f2c'
     buttonColorLight: '#fff', // default: '#fff'
     saveInCookies: false, // default: true,
-    label: '🌓', // default: ''
+    label: '☀️', // default: ''
     autoMatchOsTheme: true // default: true    
 }
 
